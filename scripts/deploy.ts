5629006491b87ba53088269d22d11e501ec83853
TheNeonDesert <admin@theneondesert.com>
2022-06-22 12:19:57 -0400

    harmony devnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import neonTokenContract from "../contract-artifacts/NeonToken.sol/NeonToken.json";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,12 +15,24 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  if (process.env.NEON_TOKEN_ADDRESS) {
+    // Deploy Altar
+    const Altar = await ethers.getContractFactory("Altar");
+    const altar = await Altar.deploy(process.env.NEON_TOKEN_ADDRESS);
+    await altar.deployed();
+    console.log("Altar deployed to:", altar.address);
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+    // Add Altar to MINTABLE role for Neon Token
+    const NeonToken = await ethers.getContractAt(
+      neonTokenContract.abi,
+      process.env.NEON_TOKEN_ADDRESS
+    );
+    const MINTER_ROLE = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes("MINTER_ROLE")
+    );
+    await NeonToken.grantRole(MINTER_ROLE, altar.address);
+    console.log("Minter granted on NeonToken");
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
